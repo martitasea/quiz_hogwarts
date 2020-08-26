1b2f0236d9cff05c64c7a53eefc08da192bf676a
martitasea <marta.pulido.asin@gmail.com>
2020-08-26 20:24:50 +0200

    FASE 2 OBLIGATORIA (Sólo preguntas propias) 03

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
+// --------------------------------------------------------------
 // CREAMOS UN ARRAY DE PREGUNTAS Y RESPUESTAS
+// --------------------------------------------------------------
 let questionDb = [
   {
     question: "1 ¿Qué famoso arquitecto construyó la casa de la cascada?",
     answers: ["Mies van der Rohe", "Frank Lloyd Wright", "Antoni Gaudí"],
-    image: "../img/casaCascada.jpg",
+    image: "/img/casaCascada.jpg",
     rightAnswer: 1,
   },
   {
     question: "2 ¿Cúal es el arquitecto del museo Guggenheim de Bilbao?",
     answers: ["Frank Ghery", "Norman Foster", "Rafael Moneo"],
-    image: "../img/guggenheim.jpg",
+    image: "/img/guggenheim.jpg",
     rightAnswer: 0,
   },
   {
     question: "3 ¿Qué longitud tien la Gran Muralla China?",
     answers: ["6.400 km", "3.150 km", "840 km"],
-    image: "../img/murallaChina.jpg",
+    image: "/img/murallaChina.jpg",
     rightAnswer: 0,
   },
   {
@@ -25,27 +27,27 @@ let questionDb = [
       "Casa Rietveld Schröder, de Gerrit Rietveld",
       "Case Study House 8, de  Charles y Ray Eames",
     ],
-    image: "../img/edificio01.jpg",
+    image: "/img/edificio01.jpg",
     rightAnswer: 1,
   },
   {
     question: "5 ¿Dónde nació Norman Foster?",
     answers: ["Inglaterra", "Australia", "Nueva Zelanda"],
-    image: "../img/normanFoster.jpg",
+    image: "/img/normanFoster.jpg",
     rightAnswer: 0,
   },
   {
     question:
       "6  ¿Cuál de los siguientes arquitectos no participó en eldiseño del Georges Pompidou de París?",
     answers: ["Renzo Piano", "Richard Rogers", "Norman Foster"],
-    image: "../img/pompidou.jpg",
+    image: "/img/pompidou.jpg",
     rightAnswer: 2,
   },
   {
     question:
       "7 ¿En qué año comenzó a otorgarse el premio Pritzker de arquitectura?",
     answers: ["1979", "1992", "1963"],
-    image: "../img/pritzker.png",
+    image: "/img/pritzker.png",
     rightAnswer: 0,
   },
   {
@@ -55,7 +57,7 @@ let questionDb = [
       "Filarmónica de Berlín",
       "Asamblea de Chandigarh",
     ],
-    image: "../img/chandigarh.jpg",
+    image: "/img/chandigarh.jpg",
     rightAnswer: 2,
   },
   {
@@ -65,59 +67,67 @@ let questionDb = [
       "Un ensayo sobre la escala humana aplicada a la arquitectura",
       "Una escultura ciclópea construida en Francia",
     ],
-    image: "../img/modulor.png",
+    image: "/img/modulor.png",
     rightAnswer: 1,
   },
   {
     question:
       "10 ¿Cómo se llama el triangulo que tiene todos sus lados diferentes?",
     answers: ["Isósceles", "Escaleno", "Equilátero"],
-    image: "../img/triangulo.png",
+    image: "/img/triangulo.png",
     rightAnswer: 1,
   },
 ];
 
-// FIJAMOS EL INICIO DE FALLOS Y ACIERTOS EN 0
-let aciertos = 0;
-let fallos = 0;
+// --------------------------------------------------------------
+// GENERAMOS LAS VARIABLES QUE HARÁN EL LINK CON EL DOM
+// --------------------------------------------------------------
+let container = document.getElementById("game");
+console.log(container);
+let results = document.getElementById("punctuation");
 
+// --------------------------------------------------------------
 //   CREAMOS UN ARRAY CON LAS RESPUESTAS CORRECTAS
+// --------------------------------------------------------------
 let answerDbCorrect = [];
 let numAnswers = questionDb.length;
 for (i = 0; i < numAnswers; i++) {
   answerDbCorrect.push(questionDb[i].rightAnswer);
 }
-// console.log("Las respuestas correctas son " + answerDbCorrect);
-// console.log(answerDbCorrect);
 
+// --------------------------------------------------------------
 // CREAMOS UN ARRAY VACÍO PARA IR METIENDO LAS RESPUESTAS DEL CONCURSANTE
+// --------------------------------------------------------------
 let answerDb = [];
 
-// HACEMOS QUE SE MUESTREN LAS SUCESIVAS PREGUNTAS
-let container = document.getElementById("game");
-let results = document.getElementById("punctuation");
-
+// --------------------------------------------------------------
 // PARA QUE EN EL INICIO ME MUESTRE LA PRIMERA PREGUNTA DEL ARRAY
+// --------------------------------------------------------------
 let click = 0;
 let cardAnswers = `
 <p class="question">${questionDb[click].question}</p>
-<a href="#" id="one" class="answer" onclick="cambioOne()" >a) ${questionDb[click].answers[0]}</p>
-<a href="#" id="two" class="answer" onclick="cambioTwo()">b) ${questionDb[click].answers[1]}</p>
-<a href="#" id="third" class="answer" onclick="cambioThird()">c) ${questionDb[click].answers[2]}</p>
+<a href="#" id="one" class="answer" onclick="cambio(0)" >a) ${questionDb[click].answers[0]}</a>
+<a href="#" id="two" class="answer" onclick="cambio(1)">b) ${questionDb[click].answers[1]}</a>
+<a href="#" id="third" class="answer" onclick="cambio(2)">c) ${questionDb[click].answers[2]}</a>
 <img src=${questionDb[click].image} alt="No puedo mostar la imagen" title="The quiz">
 `;
+console.log(container);
+console.log(cardAnswers);
 container.innerHTML = cardAnswers;
 
+// --------------------------------------------------------------
+// HACEMOS QUE SE MUESTREN LAS SUCESIVAS PREGUNTAS
 // HACEMOS LA FUNCIÓN QUE SE EJECUTA CUANDO SE HACE CLICK EN UNA RESPUESTA
-function cambioOne() {
+// --------------------------------------------------------------
+function cambio(index) {
   if (click < numAnswers - 1) {
     click += 1;
     cardAnswers = `
-        <p class="question">${questionDb[click].question}</p>
-        <a href="#" id="one" class="answer" onclick="cambioOne()" >a) ${questionDb[click].answers[0]}</p>
-        <a href="#" id="two" class="answer" onclick="cambioTwo()">b) ${questionDb[click].answers[1]}</p>
-        <a href="#" id="third" class="answer" onclick="cambioThird()">c) ${questionDb[click].answers[2]}</p>
-        <img src=${questionDb[click].image} alt="image">`;
+    <p class="question">${questionDb[click].question}</p>
+    <a href="#" id="one" class="answer" onclick="cambio(0)" >a) ${questionDb[click].answers[0]}</p>
+    <a href="#" id="two" class="answer" onclick="cambio(1)">b) ${questionDb[click].answers[1]}</p>
+    <a href="#" id="third" class="answer" onclick="cambio(2)">c) ${questionDb[click].answers[2]}</p>
+    <img src=${questionDb[click].image} alt="image">`;
     container.innerHTML = cardAnswers;
     answerDb[click - 1] = 0;
     // console.log("Las respuestas del concursante son " + answerDb);
@@ -130,49 +140,15 @@ function cambioOne() {
   }
 }
 
-function cambioTwo() {
-  if (click < numAnswers - 1) {
-    click += 1;
-    cardAnswers = `
-        <p class="question">${questionDb[click].question}</p>
-        <a href="#" id="one" class="answer" onclick="cambioOne()" >a) ${questionDb[click].answers[0]}</p>
-        <a href="#" id="two" class="answer" onclick="cambioTwo()">b) ${questionDb[click].answers[1]}</p>
-        <a href="#" id="third" class="answer" onclick="cambioThird()">c) ${questionDb[click].answers[2]}</p>
-        <img src=${questionDb[click].image} alt="image">`;
-    container.innerHTML = cardAnswers;
-    answerDb[click - 1] = 1;
-    // console.log("Las respuestas del concursante son " + answerDb);
-    // console.log(answerDb);
-  } else {
-    answerDb[numAnswers - 1] = 1;
-    console.log(answerDb);
-    console.log(answerDbCorrect);
-    comparar();
-  }
-}
+// --------------------------------------------------------------
+// FIJAMOS EL INICIO DE FALLOS Y ACIERTOS EN 0
+// --------------------------------------------------------------
+let aciertos = 0;
+let fallos = 0;
 
-function cambioThird() {
-  if (click < numAnswers - 1) {
-    click += 1;
-    cardAnswers = `
-        <p class="question">${questionDb[click].question}</p>
-        <a href="#" id="one" class="answer" onclick="cambioOne()" >a) ${questionDb[click].answers[0]}</p>
-        <a href="#" id="two" class="answer" onclick="cambioTwo()">b) ${questionDb[click].answers[1]}</p>
-        <a href="#" id="third" class="answer" onclick="cambioThird()">c) ${questionDb[click].answers[2]}</p>
-        <img src=${questionDb[click].image} alt="image">`;
-    container.innerHTML = cardAnswers;
-    answerDb[click - 1] = 2;
-    // console.log("Las respuestas del concursante son " + answerDb);
-    // console.log(answerDb);
-  } else {
-    answerDb[numAnswers - 1] = 2;
-    console.log(answerDb);
-    console.log(answerDbCorrect);
-    comparar();
-  }
-}
-
+// --------------------------------------------------------------
 // COMPARAMOS LOS RESULTADOS DEL CONCURSANTE CON LOS CORRECTOS
+// --------------------------------------------------------------
 function comparar() {
   for (i = 0; i < numAnswers; i++) {
     if (answerDbCorrect[i] == answerDb[i]) {
