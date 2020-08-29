ab00acd0cf663426391b98b683bfd1f8465a7acc
martitasea <marta.pulido.asin@gmail.com>
2020-08-29 11:56:15 +0200

    FASE 2 OBLIGATORIA (Sólo preguntas propias) 04

--- js/index.js
+++ js/index.js
@@ -5,19 +5,19 @@ let questionDb = [
   {
     question: "1 ¿Qué famoso arquitecto construyó la casa de la cascada?",
     answers: ["Mies van der Rohe", "Frank Lloyd Wright", "Antoni Gaudí"],
-    image: "/img/casaCascada.jpg",
+    image: "/quiz/img/casaCascada.jpg",
     rightAnswer: 1,
   },
   {
     question: "2 ¿Cúal es el arquitecto del museo Guggenheim de Bilbao?",
     answers: ["Frank Ghery", "Norman Foster", "Rafael Moneo"],
-    image: "/img/guggenheim.jpg",
+    image: "/quiz/img/guggenheim.jpg",
     rightAnswer: 0,
   },
   {
     question: "3 ¿Qué longitud tien la Gran Muralla China?",
     answers: ["6.400 km", "3.150 km", "840 km"],
-    image: "/img/murallaChina.jpg",
+    image: "/quiz/img/murallaChina.jpg",
     rightAnswer: 0,
   },
   {
@@ -27,27 +27,27 @@ let questionDb = [
       "Casa Rietveld Schröder, de Gerrit Rietveld",
       "Case Study House 8, de  Charles y Ray Eames",
     ],
-    image: "/img/edificio01.jpg",
+    image: "/quiz/img/edificio01.jpg",
     rightAnswer: 1,
   },
   {
     question: "5 ¿Dónde nació Norman Foster?",
     answers: ["Inglaterra", "Australia", "Nueva Zelanda"],
-    image: "/img/normanFoster.jpg",
+    image: "/quiz/img/normanFoster.jpg",
     rightAnswer: 0,
   },
   {
     question:
       "6  ¿Cuál de los siguientes arquitectos no participó en eldiseño del Georges Pompidou de París?",
     answers: ["Renzo Piano", "Richard Rogers", "Norman Foster"],
-    image: "/img/pompidou.jpg",
+    image: "/quiz/img/pompidou.jpg",
     rightAnswer: 2,
   },
   {
     question:
       "7 ¿En qué año comenzó a otorgarse el premio Pritzker de arquitectura?",
     answers: ["1979", "1992", "1963"],
-    image: "/img/pritzker.png",
+    image: "/quiz/img/pritzker.png",
     rightAnswer: 0,
   },
   {
@@ -57,7 +57,7 @@ let questionDb = [
       "Filarmónica de Berlín",
       "Asamblea de Chandigarh",
     ],
-    image: "/img/chandigarh.jpg",
+    image: "/quiz/img/chandigarh.jpg",
     rightAnswer: 2,
   },
   {
@@ -67,14 +67,14 @@ let questionDb = [
       "Un ensayo sobre la escala humana aplicada a la arquitectura",
       "Una escultura ciclópea construida en Francia",
     ],
-    image: "/img/modulor.png",
+    image: "/quiz/img/modulor.png",
     rightAnswer: 1,
   },
   {
     question:
       "10 ¿Cómo se llama el triangulo que tiene todos sus lados diferentes?",
     answers: ["Isósceles", "Escaleno", "Equilátero"],
-    image: "/img/triangulo.png",
+    image: "/quiz/img/triangulo.png",
     rightAnswer: 1,
   },
 ];
@@ -124,9 +124,9 @@ function cambio(index) {
     click += 1;
     cardAnswers = `
     <p class="question">${questionDb[click].question}</p>
-    <a href="#" id="one" class="answer" onclick="cambio(0)" >a) ${questionDb[click].answers[0]}</p>
-    <a href="#" id="two" class="answer" onclick="cambio(1)">b) ${questionDb[click].answers[1]}</p>
-    <a href="#" id="third" class="answer" onclick="cambio(2)">c) ${questionDb[click].answers[2]}</p>
+    <a href="#" id="one" class="answer" onclick="cambio(0)" >a) ${questionDb[click].answers[0]}</a>
+    <a href="#" id="two" class="answer" onclick="cambio(1)">b) ${questionDb[click].answers[1]}</a>
+    <a href="#" id="third" class="answer" onclick="cambio(2)">c) ${questionDb[click].answers[2]}</a>
     <img src=${questionDb[click].image} alt="image">`;
     container.innerHTML = cardAnswers;
     answerDb[click - 1] = 0;
